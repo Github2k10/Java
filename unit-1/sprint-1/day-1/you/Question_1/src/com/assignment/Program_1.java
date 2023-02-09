0c2835a4a89604c1cd4a32e6ec7a5f392587b959
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-09 23:52:37 +0530

    update

---
+++ unit-1/sprint-1/day-1/you/Question_1/src/com/assignment/Program_1.java
@@ -0,0 +1,26 @@
+package com.assignment;
+
+public class Program_1 {
+	public static void main(String args[]) {
+		
+		
+		//1) Write a java application that will print Your Name and Address in the following pattern:
+		
+		System.out.println("This is My First Java Application");
+		System.out.println("==================================");
+		System.out.println("My Name is: Ankit Kumar");
+		System.out.println("My Address is: New Delhi, India");
+		System.out.println();
+		System.out.println("-------------------------------------------------");
+		System.out.println();
+		
+		
+		
+		//2)Try to execute following code on your computer and observe the difference between System.out.println() and System.out.print() & use of \n.
+		System.out.print("Hello");
+		System.out.print("World");
+		System.out.println();
+		System.out.println("Hello");
+		System.out.println("World");
+	}
+}
