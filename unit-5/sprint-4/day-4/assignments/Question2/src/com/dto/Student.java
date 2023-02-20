3c7fbfb4484ae4ac7f54a0579f641952c832fb2c
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-20 09:58:32 +0530

    update

---
+++ unit-5/sprint-4/day-4/assignments/Question2/src/com/dto/Student.java
@@ -0,0 +1,20 @@
+package com.dto;
+
+public interface Student {
+	public int getRollNo();
+	
+	public void setRollNo(int rollNo);
+	
+	public String getStudentName();
+	
+	public void setStudentName(String studentName);
+	
+	public String getAddress();
+	
+	public void setAddress(String address);
+	
+	public int getMarks();
+	
+	public void setMarks(int marks);
+	
+}
